2c93fc163b952f5fd366d06797fe1a33edf9b1de
Skillcode-ML <84472319+Skillcode-ML@users.noreply.github.com>
2021-05-28 23:38:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,111 +1,63 @@
 # EDA
 import streamlit as st 
+import pyttsx3
 
-# EDA Pkgs
-import pandas as pd 
-import numpy as np 
-
-
-# Data Viz Pkg
-import matplotlib.pyplot as plt 
-import matplotlib
-matplotlib.use("Agg")
-import seaborn as sns 
 
 
 
 def main():
-	"""Semi Automated ML App with Streamlit """
+	boolna = 0
+	onpage = 0
+	type = [".txt",".pdf"]	
+	choice = st.sidebar.selectbox("Select the type of file",type)
 
-	activities = ["EDA","Plots"]	
-	choice = st.sidebar.selectbox("Select Activities",activities)
+	if choice == '.txt':
+		st.subheader("Reading text file")
 
-	if choice == 'EDA':
-		st.subheader("Exploratory Data Analysis")
-
-		data = st.file_uploader("Upload a Dataset", type=["csv", "txt"])
+		data = st.file_uploader("Upload a text file", type=["txt"])
 		if data is not None:
-			df = pd.read_csv(data)
-			st.dataframe(df.head())
+			df = pd.read_txt(data)
 			
-
-			if st.checkbox("Show Shape"):
-				st.write(df.shape)
-
-			if st.checkbox("Show Columns"):
-				all_columns = df.columns.to_list()
-				st.write(all_columns)
-
-			if st.checkbox("Summary"):
-				st.write(df.describe())
-
-			if st.checkbox("Show Selected Columns"):
-				selected_columns = st.multiselect("Select Columns",all_columns)
-				new_df = df[selected_columns]
-				st.dataframe(new_df)
-
-			if st.checkbox("Show Value Counts"):
-				st.write(df.iloc[:,-1].value_counts())
-
-			if st.checkbox("Correlation Plot(Matplotlib)"):
-				plt.matshow(df.corr())
-				st.pyplot()
-
-			if st.checkbox("Correlation Plot(Seaborn)"):
-				st.write(sns.heatmap(df.corr(),annot=True))
-				st.pyplot()
-
-
-			if st.checkbox("Pie Plot"):
-				all_columns = df.columns.to_list()
-				column_to_plot = st.selectbox("Select 1 Column",all_columns)
-				pie_plot = df[column_to_plot].value_counts().plot.pie(autopct="%1.1f%%")
-				st.write(pie_plot)
-				st.pyplot()
-
-
-
-	elif choice == 'Plots':
-		st.subheader("Data Visualization")
-		data = st.file_uploader("Upload a Dataset", type=["csv", "txt", "xlsx"])
-		if data is not None:
-			df = pd.read_csv(data)
-			st.dataframe(df.head())
-
-
-			if st.checkbox("Show Value Counts"):
-				st.write(df.iloc[:,-1].value_counts().plot(kind='bar'))
-				st.pyplot()
+			volume = st.slider('Volume',0,100,25,1)
+			rate = st.slider('Rate',0,100,85,1)
+			engine = pyttsx3.init()
+			engine.setProperty('rate', rate)
+			engine.setProperty('volume', volume)
+			engine.say(text)
+			engine.runAndWait()
+			
+				
 		
-			# Customizable Plot
+	if choice == '.pdf':
+		st.subheader("Reading pdf file")
 
-			all_columns_names = df.columns.tolist()
-			type_of_plot = st.selectbox("Select Type of Plot",["area","bar","line","hist","box","kde"])
-			selected_columns_names = st.multiselect("Select Columns To Plot",all_columns_names)
+		data = st.file_uploader("Upload a pdf file", type=["pdf"])
+		if data is not None:
+			pdfreader = PyPDF2.PdfFileReader(data)
+			pages = pdfreader.numPages()
+			
+			volume = st.slider('Volume',0,100,25,1)
+			rate = st.slider('Rate',0,100,45,1)
+			
+			if st.button("Play"):
+				boolna = 2
+				playy(volume, rate, onpage)
+			if st.button("Pause"):
+				boolna = 1
+			if st.button("Stop"):
+				boolna = 0
+				onpage = 0
+def playy(vol, r, pgno)
+	while boolna==2:
+		for num in range(pgno, pages)
+			page = pdfreader.getPage(pgno)
+			tex = page.extractText()
+			player = pyttsx3.init()
+			player.setProperty('rate', r)
+			player.setProperty('volume', vol)
+			player.say(text)
+			player.runAndWait()
+			onpage = pgno
+		st.write("Finished")
 
-			if st.button("Generate Plot"):
-				st.success("Generating Customizable Plot of {} for {}".format(type_of_plot,selected_columns_names))
-
-				# Plot By Streamlit
-				if type_of_plot == 'area':
-					cust_data = df[selected_columns_names]
-					st.area_chart(cust_data)
-
-				elif type_of_plot == 'bar':
-					cust_data = df[selected_columns_names]
-					st.bar_chart(cust_data)
-
-				elif type_of_plot == 'line':
-					cust_data = df[selected_columns_names]
-					st.line_chart(cust_data)
-
-				# Custom Plot 
-				elif type_of_plot:
-					cust_plot= df[selected_columns_names].plot(kind=type_of_plot)
-					st.write(cust_plot)
-					st.pyplot()
-    
-
-
-if __name__ == '__main__':
-	main()
+main()
